29b4bb7c6ded0bcd281151ee474db0819d90f5b7
Hammania689 <hameedabdulrashid@gmail.com>
2018-12-02 09:06:49 -0600

    Test deploy

--- yelp4geeks/settings.py
+++ yelp4geeks/settings.py
@@ -126,6 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/assets'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
